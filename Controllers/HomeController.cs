58c5a3fb1cdb234a5edcb40523c94f3ba437a633
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-08-17 22:48:04 +0200

    Added a Service
    - Added a service interface IVideoData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VideoAsp.Models;
+using System.Collections.Generic;
 
 namespace VideoAsp.Controllers
 {
@@ -20,7 +21,12 @@ namespace VideoAsp.Controllers
 
         public ViewResult Index()
         {
-            var model = new Video { Id = 1, Title = "Shreck" };
+            var model = new List<Video>
+            {
+                new Video { Id = 1, Title = "Shreck" },
+                new Video { Id = 2, Title = "Despicable Me" },
+                new Video { Id = 3, Title = "Megamind" }
+            };
             return View(model);
         }
 
