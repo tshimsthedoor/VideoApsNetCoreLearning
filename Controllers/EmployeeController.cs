75de4fe958493ed8753d037523abd6411a1b9fd8
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-08-17 22:27:43 +0200

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoAsp.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class EmployeeController : Controller
+    {
+       
+        
+        public string Index()
+        {
+            return "Hello from Employee";
+        }
+
+        
+        public ContentResult Name()
+        {
+            return Content("Jonas");
+        }
+
+       
+        public string Country()
+        {
+            return "Sweden";
+        }
+        
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VideoAsp.Models;
+
+namespace VideoAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ObjectResult Index()
+        {
+            var model = new Video { Id = 1, Title = "Shreck" };
+            return new ObjectResult(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
